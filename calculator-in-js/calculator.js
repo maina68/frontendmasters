bed73595c9e6b141298547ff823663798b69c007
maina george <98666205+maina68@users.noreply.github.com>
2024-07-08 18:09:43 +0000

    chore: Refactor handleMath function in calculator.js to improve math operations

--- calculator-in-js/calculator.js
+++ calculator-in-js/calculator.js
@@ -1,4 +1,6 @@
 let buffer = "0";
+let runningTotal = 0;
+let previousOperator = null;
 const screen = document.querySelector('.screen');
 
 function buttonClick(value) {
@@ -18,8 +20,64 @@ function handleNumber(number){
     }
 }
 
+
+function handleMath(value) {
+     if (buffer === "0") {
+        // do nothing
+        return;
+     }
+        const intBuffer = parseInt(buffer);
+        if (runningTotal === 0) {
+            runningTotal = intBuffer;
+        } else {
+            flushOperation(intBuffer);
+        }
+        previousOperator = value;
+        buffer = "0";
+        console.log(runningTotal);
+}
+
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer;
+    } else if (previousOperator === '÷') {
+        runningTotal /= intBuffer;
+    }
+
+}
 function handleSymbol(symbol){
-    console.log('symbol');
+    switch(symbol) {
+        case 'C':
+            buffer = "0";
+            break;
+        case '=':
+            if (previousOperator === null) {
+                // need two numbers to do math
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;    
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+    }
 }
 
 function innit() {
